34c05bad7444bb93e8e12305ef33c3a7c6ab5370
Abner Santana <FineLine>
2020-03-31 17:08:15 -0300

    base com movimentacao e salto
    Esta versao permite o objeto navegar sobre a janela transladando e pulando

--- BoxAttack.py
+++ BoxAttack.py
@@ -1,13 +1,19 @@
 # KidsCanCode - Game Development with Pygame video series
-# Shmup game - part 1
-# Video link: https://www.youtube.com/watch?v=nGufy7weyGY
-# Player sprite and movement
+# Shmup game - part 2
+# Video link: https://www.youtube.com/watch?v=-5GNbL33hz0
+# Enemy sprites
 import pygame
+import pygame as pg
 import random
+vec = pg.math.Vector2
 
 WIDTH = 800
 HEIGHT = 600
 FPS = 60
+
+# Player Configurations
+PLAYER_ACC = 0.5
+
 
 # define colors
 WHITE = (255, 255, 255)
@@ -21,7 +27,7 @@ YELLOW = (255, 255, 0)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Stack Attack!")
+pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
 class Player(pygame.sprite.Sprite):
@@ -31,25 +37,50 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 50
+        self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.speedy = 0
 
     def update(self):
         self.speedx = 0
+        self.speedy = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-            self.speedx = -6
+            self.speedx = -8
         if keystate[pygame.K_RIGHT]:
-            self.speedx = 6
+            self.speedx = 8
+        if keystate[pygame.K_UP]:
+            self.speedy = -8
+        self.rect.y += self.speedy
         self.rect.x += self.speedx
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
 
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+for i in range(20):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 
 # Game loop
 running = True
